a36555d7d27746905c47daccf99d69468740dfe5
dduohereshui <2245675739@qq.com>
2022-10-04 00:14:27 +0800

    monkey thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
 import homeReducer from "./home";
 import aboutReducer from "./about";
 
@@ -9,18 +9,33 @@ const store = createStore(
     home: homeReducer,
     about: aboutReducer,
   }),
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers()
 );
 
 // monkey patch
-function log(store) {
-  const dispatch = store.dispatch;
-  function logAndDispatch(action) {
-    console.log("action: ", action);
-    dispatch(action);
-    console.log("派发结果: ", store.getState());
+// function log(store) {
+//   const dispatch = store.dispatch;
+//   function logAndDispatch(action) {
+//     console.log("action: ", action);
+//     dispatch(action);
+//     console.log("派发结果: ", store.getState());
+//   }
+//   store.dispatch = logAndDispatch;
+// }
+// log(store);
+
+function thunk(store) {
+  const next = store.dispatch;
+  function dispatchThunk(action) {
+    if (typeof action === "function") {
+      // 使用新的dispatch
+      action(store.dispatch, store.getState);
+      console.log("custom thunk");
+    } else {
+      next(action);
+    }
   }
-  store.dispatch = logAndDispatch;
+  store.dispatch = dispatchThunk;
 }
-log(store);
+thunk(store);
 export default store;
